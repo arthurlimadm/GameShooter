6a0e1b316f5bb050561dd867dca613ec9a9a620b
Arthur Lima <98204950+arthurlimadm@users.noreply.github.com>
2022-12-26 13:17:11 -0300

    Delete client/src directory

--- client/src/helpers/CanvasConfig.ts
+++
@@ -1,9 +0,0 @@
-export const canvasConfig = (canvas:HTMLCanvasElement, context:any)=>{
-    const dpr = window.devicePixelRatio;
-    const rect = canvas.getBoundingClientRect();
-    canvas.width = rect.width * dpr;
-    canvas.height = rect.height * dpr;
-    context.scale(dpr, dpr);
-    canvas.style.width = `${rect.width}px`;
-    canvas.style.height = `${rect.height}px`;
-}
