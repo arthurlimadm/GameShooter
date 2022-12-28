431d5ccaa1324f8b5f30e3745d11e4ab1b6f5e0b
Arthur Lima <98204950+arthurlimadm@users.noreply.github.com>
2022-12-28 13:07:58 -0300

    Delete client/src directory

--- client/src/helpers/GenerateRandomEnemies.ts
+++
@@ -1,9 +0,0 @@
-import { Ghost } from "../entities/Ghost";
-import { Sprite } from "../entities/Sprite";  
-
-
-export const GenerateRandomEnemies = (context:CanvasRenderingContext2D):Sprite=>{
-    const {width, height} = window.screen
-
-    return new Ghost(context, '', {x: Math.random() * width/2, y: Math.random() * height / 2}, {width: 100, height: 100})
-}
